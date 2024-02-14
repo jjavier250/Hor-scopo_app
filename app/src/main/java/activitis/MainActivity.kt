7c6a3b022e212277dedff5cc8e4f54b66288ae8c
Javi <jegido250@gmail.com>
2024-02-14 12:17:00 +0100

    inicio proyecto no se

---
+++ app/src/main/java/activitis/MainActivity.kt
@@ -0,0 +1,12 @@
+package activitis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.horoscopo_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
